278f8c7ef7df8bc1221dfc6054669679980d512e
Marlon Gómez <marlonjulian15@gmail.com>
2021-06-08 13:54:42 -0500

    Add config env

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-const API_VERSION = "v1";
-const HOST = "localhost";
-const USER = "root";
-const PASSWORD = "";
-const DATABASE = "webcredito_database";
-const PORT_DB = 27017;
+require("dotenv").config();
+
+const API_VERSION = process.env.API_VERSION;
+const HOST = process.env.HOST;
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
+const PORT_DB = process.env.PORT_DB;
 module.exports = {
   API_VERSION,
   HOST,
